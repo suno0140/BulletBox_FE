1b0e3438dca337dc2616db7a49c0c89d3e3c2eae
suno0140 <vlfxhd15@naver.com>
2023-09-15 23:56:46 +0900

    DailyLog getTodo

--- src/pages/DailyLog.tsx
+++ src/pages/DailyLog.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   DateContainer,
   FlexContainer,
@@ -9,28 +9,41 @@ import { AddCategoryIcon, CategoryAddBtn } from '@components/atoms/Icon';
 import MainTodoCard from '@components/molecules/MainTodoCard';
 import useCurrentDate from '@hooks/useCurrentData';
 import { usePageLocation } from '@hooks/usePageLocation';
+import { useRequest } from '@hooks/useRequest';
+import { getTodoApi } from '@api/TodoApi';
 
 const DailyLog = () => {
   const [reload, setReload] = React.useState(false);
+  const { data: todosData, request: getTodos } = useRequest({
+    apiFunc: getTodoApi,
+    reduxKey: 'todos',
+  });
 
   const currentDate = useCurrentDate();
 
   const { goToDailyLogAdd } = usePageLocation();
+
+  useEffect(() => {
+    getTodos();
+  }, []);
 
   return (
     <>
       <MainPageContaiver>
         <DateContainer>{currentDate}</DateContainer>
         <MainTodoContainer>
-          {/* {todos.map((todo) => (
-            <MainTodoCard
-              key={todo.id}
-              todoId={todo.id}
-              todoContent={todo.todo}
-              time={null}
-              color={todo.color}
-            />
-          ))} */}
+          {todosData &&
+            Object.entries(todosData).map(
+              ([todoId, todoItem]: [string, any]) => (
+                <MainTodoCard
+                  key={todoId}
+                  todoId={todoId}
+                  todoContent={todoItem.todo}
+                  time={null}
+                  color={todoItem.color}
+                />
+              ),
+            )}
         </MainTodoContainer>
 
         <FlexContainer>
--- src/pages/Diary.tsx
+++ src/pages/Diary.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { MainCalendar } from '@components/molecules/Calendar';
 import useCurrentDate from '@hooks/useCurrentData';
 
